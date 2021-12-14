7a0625cb0fcb03103f1b4dc78d19d355142456b2
Bilal <yyybilal@gmail.com>
2021-12-14 10:51:57 +0100

    Initial Commit

---
+++ 64 fenetres/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  64 fenetres
+//
+//  Created by bilal on 14/12/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 64 fenetres/_4_fenetresApp.swift
@@ -0,0 +1,17 @@
+//
+//  _4_fenetresApp.swift
+//  64 fenetres
+//
+//  Created by bilal on 14/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct _4_fenetresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
